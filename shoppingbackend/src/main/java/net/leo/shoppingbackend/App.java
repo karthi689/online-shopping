47397392df13db0d10e3e4b0298a44a63d322284
leo <karthik14.me@gmail.com>
2018-06-19 20:27:56 -0400

    My Project Setup Completed Successfully.

---
+++ shoppingbackend/src/main/java/net/leo/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.leo.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
