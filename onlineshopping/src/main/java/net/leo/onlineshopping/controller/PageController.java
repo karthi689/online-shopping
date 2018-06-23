a28a251f22300e2efa0ba085b5711a3170602014
leo <karthik14.me@gmail.com>
2018-06-23 01:00:23 -0400

    Successfully Loaded Static Resources

--- onlineshopping/src/main/java/net/leo/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/leo/onlineshopping/controller/PageController.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	
-	@RequestMapping(value = {"/", "/home", "/index"})
+
+	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring Web MVC");
